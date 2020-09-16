c5cc9ab69e37c31ead83d0c0626b1633ed4fbcbd
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-09-16 06:37:41 -0500

    feat (server): Aded observer configuration struct

--- server/observer.go
+++ server/observer.go
@@ -1,1 +1,9 @@
 package server
+
+// ObserverConf wraps the observer configuration
+type ObserverConf struct {
+	SupersenseBuffer  string
+	SupersenseTitle   string
+	SupersenseHashtag string
+	SupersenseBrand   string
+}
